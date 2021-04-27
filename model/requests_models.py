1d4853d6bc139a1cf0959623b5da82f1258dfc1d
amArbaoui <adel.arbaoui@gmail.com>
2021-04-27 12:40:41 +0000

    модели в отдельный модуль

---
+++ model/requests_models.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+from typing import List
+
+import datetime as dt
+
+
+class ParsingParams(BaseModel):
+    per_start: dt.date
+    per_end: dt.date
+    comp_list: List[str]
+
+
+class EmiHotlineParams(BaseModel):
+    per_start: dt.date
+    per_end: dt.date
+    comp_list: List[str]
+
+
+class PricesZakupParams(BaseModel):
+    partners: List[str]
+    per_dates: List[dt.date]
+
+
+class DopAddition(BaseModel):
+    dop_sebest: float
+    dop_indicativ: float
+    dop_vxod_1: float
+    dop_vxod_2: float
+    dop_mrc: float
+    dop_poruchenie: float
+    dop_ustanovki: float
+    dop_parsing: float
+    dop_rinok: float
+    kz_priplata_1: float
+    kz_priplata_2: float
+
+
+class PriceCalcParams(BaseModel):
+    div: str
+    gfu: str
+    emi_price_hotline: EmiHotlineParams
+    parsing: ParsingParams
+    p_zakup: PricesZakupParams
+    priplati: DopAddition
